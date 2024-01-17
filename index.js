5f5a9276cc8e5b0befe152b0d64cac87ed4b75d4
amrata <soniamrata55@gmail.com>
2024-01-17 19:42:05 +0530

    this task is completed now

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const axios = require("axios")
 
 app.use(express.json())
 
+
 app.post('/datas', async (req, res) => {
     try {
-      const value = req.body;
-      const response = await axios.post("https://chimpu.xyz/api/post.php", 
+      const value = req.query.phonenumber;
+      const response = await axios.post('https://chimpu.xyz/api/post.php', 
      { phonenumber:7355675147}
-    );
-    //  const headers = req.headers;
+      )
     const headers = response.headers;
-    console.log('Response Headers:', headers);
-
-    res.json({data:response.data, Headers:headers});
+    
+    res.status(200).send({data:response.data, Headers:headers});
 } catch (error) {
   console.error('Error:', error.message);
   res.status(500).send('Internal Server Error');
