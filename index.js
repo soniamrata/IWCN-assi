bcc825d724dbc36132299c80b5869a71456f446b
amrata <soniamrata55@gmail.com>
2024-01-17 22:30:46 +0530

    completed finally

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
+const axios = require("axios")
 const express = require("express")
 const app = express()
-const axios = require("axios")
 
-
-app.use(express.json())
-
-
-app.post('/datas', async (req, res) => {
+app.get('/datas', async (req, res) => {
     try {
       const value = req.query.phonenumber;
       const response = await axios.post('https://chimpu.xyz/api/post.php', 
-     { phonenumber:7355675147}
+      { phonenumber:value}
       )
     const headers = response.headers;
-    
+    console.log(headers);
     res.status(200).send({data:response.data, Headers:headers});
 } catch (error) {
   console.error('Error:', error.message);
   res.status(500).send('Internal Server Error');
 }
 });
-
 app.listen(3000,function(){
     console.log("MongoDB running on port 3000")
 })
